9077af3ebac917a61f478bfb75bf9d8456557fd3
kishore7AI <kishoresabari007@gmail.com>
2025-04-07 22:46:57 +0530

    promise.js

--- promise.js
+++ promise.js
@@ -1,13 +1,36 @@
-function fetch() {
-    return new Promise((resolve)=>{
+const data= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        const ans="kishore";
+        if(ans){
+            resolve(ans);
+        }else{
+            reject('no data');
+        }
+    },3000);
+});
+function getuserDetails(name){
+    const userDetails= new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            console.log("manoj")
-        },2000)
+            let userDetails=[{name:"kishore",age:"30"}]
+            const user= userDetails.find((user)=>user.name==name)
+            if(user){
+                resolve(user)
+            }else{
+                reject('nothing')
+            }
+        },3000);
     });
+    return userDetails;
 }
-  async function get(){
-    console.log("kishore...");
-    let a = await fetch();
-    console.log(a)
-  }
-  get()
+data.then((name)=>{
+    return getuserDetails(name)
+})
+.then((userDetail)=>{
+    console.log(userDetail)
+})
+.catch((error)=>{
+    console.log(error.name)
+})
+.finally(()=>{
+    console.log("last")
+})
